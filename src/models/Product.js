ea63aac13dc22e4186b37f9de07f17422cb1c574
BillyStudy <billystudy@hotmail.com>
2020-02-17 15:59:25 -0300

    Pagination

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate')
 
 
 const ProductSchema = new mongoose.Schema({
@@ -22,6 +23,8 @@ const ProductSchema = new mongoose.Schema({
 
 
 });
+
+ProductSchema.plugin(mongoosePaginate)
 
 
 mongoose.model('Product', ProductSchema);
