8ac579b258c5ae5f4ab2be0f01566b9af5e29daf
BillyStudy <billystudy@hotmail.com>
2020-02-17 15:05:15 -0300

    Creating product model

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //imports
 const express = require('express');
 const mongoose = require('mongoose');
+const requiredir = require('require-dir');
 
     // inicia o app
     const app = express();
 
     //iniciando o database
     mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true,  useNewUrlParser: true });
+    requiredir('./src/models')
 
+    const Product = mongoose.model('Product');
     //rotas
     app.get('/', (rec, res) => {
-        res.send('Hello world');
+        Product.create({title:"Google", description:"Site de pesquisas", url:"google.com"})
+        return res.send('Hello world');
     });
 
     //escolhendo a porta a ser ouvida
