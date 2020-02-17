82feef1a18f8b36c775a605c0461165f32117d46
BillyStudy <billystudy@hotmail.com>
2020-02-17 16:02:38 -0300

    CORS

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requiredir = require('require-dir');
+const cors = require('cors')
+
 
     // inicia o app
     const app = express();
     app.use(express.json());
+    app.use(cors());
 
     //iniciando o database
     mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true,  useNewUrlParser: true });
