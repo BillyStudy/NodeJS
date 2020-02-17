bf9fa0f3dd301f1fc29fb3b7ab87004407fe5eb6
BillyStudy <billystudy@hotmail.com>
2020-02-17 15:20:12 -0300

    File restructuring

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ const requiredir = require('require-dir');
     mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true,  useNewUrlParser: true });
     requiredir('./src/models')
 
-    const Product = mongoose.model('Product');
     //rotas
-    app.get('/', (rec, res) => {
-        Product.create({title:"Google", description:"Site de pesquisas", url:"google.com"})
-        return res.send('Hello world');
-    });
+    app.use('/api', require('./src/routes'));
 
     //escolhendo a porta a ser ouvida
     app.listen(3001);
